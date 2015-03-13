f52655ab3c61c59f8861fb5cea773c01b48c7a6b
Alex Smith <iqwz@ya.ru>
2015-03-13 23:08:56 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,14 @@
+My most crazy program! Dynamic (you can choose the length, width, and with material) to build a house using turtles from the [ComputerCraft mod][compcraft] in MineCraft.
+The program is written in the built-in scripting language Lua, api to control the turtle [here][turtleAPI]  
+
+![s1]  
+![s2]  
+![s3]  
+![s4]  
+
+[compcraft]: http://www.computercraft.info/
+[turtleAPI]: http://computercraft.info/wiki/Turtle_%28API%29
+[s1]: ./screenshots/1.jpg
+[s2]: ./screenshots/2.jpg
+[s3]: ./screenshots/3.jpg
+[s4]: ./screenshots/4.jpg
---
+++ fastHouse.lua
@@ -0,0 +1,421 @@
+--[[
+  The MIT License (MIT)
+  Copyright (c) 2015 Александр Смит (https://github.com/DarkScorpion)
+  
+  Tortle build house from 2 material and glass panel
+  Need 3 stack of wall material
+       3 stack glass panel
+       5 stack of roof materials
+       1000 fueld in turtle
+  (!)Need place wall material in 1 slot, panel in 2, roof material in 3.
+  (!)Materials to supplement slots 4-16 in any order
+]]
+-- config data
+_width = 15
+_length = 19
+
+_slotStone = 1
+_slotGlass = 2
+_slotRoof = 3
+
+_fillStartSlot = 4
+-- end config data
+
+function fillMaterial(slot)
+  if turtle.getItemSpace(slot) == 0 then
+    return
+  end
+
+  turtle.select(slot)
+  for i=_fillStartSlot, 16 do
+    if turtle.compareTo(i) then
+      if turtle.getItemCount(i) > turtle.getItemSpace(slot) then
+        turtle.select(i)
+        turtle.transferTo(slot, turtle.getItemSpace(slot))
+        turtle.select(slot)
+      else
+        turtle.select(i)
+        turtle.transferTo(slot, turtle.getItemCount(i))
+        turtle.select(slot)
+      end
+      
+    end --end for
+
+    if turtle.getItemSpace(slot) == 0 then
+      break
+    end
+
+  end
+end -- end fillMaterial
+
+function refillAll()
+  for i=1,3 do
+    fillMaterial(i)
+  end
+end -- end refillAll
+
+function buildLayer1(length, width)
+  
+  local lengthMiddle = math.floor(length/2)+1
+  local widthMiddle = math.floor(width/2)+1
+
+  local i
+  refillAll()
+  
+  turtle.up()
+  --Left side of house
+  turtle.select(_slotStone)
+  for i=1, width do
+    if i ~= widthMiddle then
+      turtle.placeDown()
+    end
+
+    if i ~= width then
+      turtle.forward()
+    end
+  end
+
+  print(turtle.getFuelLevel())
+  --back side of house
+  turtle.turnRight()
+  for i=1, length do
+    if (i~=1 and i~=lengthMiddle) then
+      turtle.placeDown()
+    end
+
+    if i ~= length then
+      turtle.forward()
+    end
+  end
+
+  print(turtle.getFuelLevel())
+  --right side of house
+  turtle.turnRight()
+  for i=1, width do
+    
+    if (i~=1 and i~=widthMiddle) then
+      turtle.placeDown()
+    end
+
+    if i ~= width then
+      turtle.forward()
+    end
+
+  end
+
+  refillAll()
+  print(turtle.getFuelLevel())
+  --forward side of house
+  turtle.turnRight()
+  for i=1, length do
+    if (i~=1 and i~=lengthMiddle and i~=length) then
+      turtle.placeDown()
+    end
+
+    if i ~= length then
+      turtle.forward()
+    end
+  end
+
+  turtle.turnRight()
+end -- end layer1 
+
+
+function buildLayer2(length, width)
+  
+  local lengthMiddle = math.floor(length/2)+1
+  local widthMiddle = math.floor(width/2)+1
+
+  local i
+  refillAll()
+
+  turtle.up()
+  --Left side of house
+  turtle.select(_slotStone)
+  for i=1, width do
+    if i ~= widthMiddle then
+      if (i==1 or i==widthMiddle-1 or i==widthMiddle+1 or i==width) then
+        turtle.select(_slotStone)
+        turtle.placeDown()
+      else
+        turtle.select(_slotGlass)
+        turtle.placeDown()
+      end
+    end
+
+    if i ~= width then
+      turtle.forward()
+    end
+  end
+
+  print(turtle.getFuelLevel())
+  --back side of house
+  turtle.turnRight()
+  for i=1, length do
+    if (i~=1 and i~=lengthMiddle) then
+      if (i==1 or i==lengthMiddle-1 or i==lengthMiddle+1 or i==length) then
+        turtle.select(_slotStone)
+        turtle.placeDown()
+      else
+        turtle.select(_slotGlass)
+        turtle.placeDown()
+      end
+    end
+
+    if i ~= length then
+      turtle.forward()
+    end
+  end
+
+  refillAll()
+  print(turtle.getFuelLevel())
+  --right side of house
+  turtle.turnRight()
+  for i=1, width do
+    
+    if (i~=1 and i~=widthMiddle) then
+      if (i==1 or i==widthMiddle-1 or i==widthMiddle+1 or i==width) then
+        turtle.select(_slotStone)
+        turtle.placeDown()
+      else
+        turtle.select(_slotGlass)
+        turtle.placeDown()
+      end
+    end
+
+    if i ~= width then
+      turtle.forward()
+    end
+
+  end
+
+  print(turtle.getFuelLevel())
+  --forward side of house
+  turtle.turnRight()
+  for i=1, length do
+    if (i~=1 and i~=lengthMiddle and i~=length) then
+      if (i==1 or i==lengthMiddle-1 or i==lengthMiddle+1 or i==length) then
+        turtle.select(_slotStone)
+        turtle.placeDown()
+      else
+        turtle.select(_slotGlass)
+        turtle.placeDown()
+      end
+    end
+
+    if i ~= length then
+      turtle.forward()
+    end
+  end
+
+  turtle.turnRight()
+end -- end layer2
+
+function buildLayer3(length, width)
+  
+  local lengthMiddle = math.floor(length/2)+1
+  local widthMiddle = math.floor(width/2)+1
+
+  local i
+  refillAll()
+
+  turtle.up()
+  --Left side of house
+  turtle.select(_slotStone)
+  for i=1, width do
+
+    if (i==1 or (i>=widthMiddle-1 and i<=widthMiddle+1) or i==width) then
+      turtle.select(_slotStone)
+      turtle.placeDown()
+    else
+      turtle.select(_slotGlass)
+      turtle.placeDown()
+    end
+
+    if i ~= width then
+      turtle.forward()
+    end
+  end
+
+  print(turtle.getFuelLevel())
+  --back side of house
+  turtle.turnRight()
+  for i=1, length do
+    if (i~=1) then
+      if (i==1 or (i>=lengthMiddle-1 and i<=lengthMiddle+1) or i==length) then
+        turtle.select(_slotStone)
+        turtle.placeDown()
+      else
+        turtle.select(_slotGlass)
+        turtle.placeDown()
+      end
+    end
+
+    if i ~= length then
+      turtle.forward()
+    end
+  end
+
+  refillAll()
+  print(turtle.getFuelLevel())
+  --right side of house
+  turtle.turnRight()
+  for i=1, width do
+    
+    if (i~=1) then
+      if (i==1 or (i>=widthMiddle-1 and i<=widthMiddle+1) or i==width) then
+        turtle.select(_slotStone)
+        turtle.placeDown()
+      else
+        turtle.select(_slotGlass)
+        turtle.placeDown()
+      end
+    end
+
+    if i ~= width then
+      turtle.forward()
+    end
+
+  end
+
+  print(turtle.getFuelLevel())
+  --forward side of house
+  turtle.turnRight()
+  for i=1, length do
+    if (i~=1 and i~=length) then
+      if (i==1 or (i>=lengthMiddle-1 and i<=lengthMiddle+1) or i==length) then
+        turtle.select(_slotStone)
+        turtle.placeDown()
+      else
+        turtle.select(_slotGlass)
+        turtle.placeDown()
+      end
+    end
+
+    if i ~= length then
+      turtle.forward()
+    end
+  end
+
+  turtle.turnRight()
+end
+
+function buildLayer4(length, width)
+  
+  local lengthMiddle = math.floor(length/2)+1
+  local widthMiddle = math.floor(width/2)+1
+
+  local i
+  refillAll()
+
+  turtle.up()
+  --Left side of house
+  turtle.select(_slotStone)
+  for i=1, width do
+    
+    turtle.placeDown()
+
+    if i ~= width then
+      turtle.forward()
+    end
+  end
+
+  print(turtle.getFuelLevel())
+  --back side of house
+  turtle.turnRight()
+  for i=1, length do
+    if (i~=1) then
+      turtle.placeDown()
+    end
+
+    if i ~= length then
+      turtle.forward()
+    end
+  end
+
+  refillAll()
+  turtle.select(_slotStone)
+  print(turtle.getFuelLevel())
+  --right side of house
+  turtle.turnRight()
+  for i=1, width do
+    
+    if (i~=1) then
+      turtle.placeDown()
+    end
+
+    if i ~= width then
+      turtle.forward()
+    end
+
+  end
+
+  print(turtle.getFuelLevel())
+  --forward side of house
+  turtle.turnRight()
+  for i=1, length do
+    if (i~=1 and i~=length) then
+      turtle.placeDown()
+    end
+
+    if i ~= length then
+      turtle.forward()
+    end
+  end
+
+  turtle.turnRight()
+end -- endLayer4
+
+function buildRoof(length, width)
+  
+  turtle.up()
+
+  refillAll()
+  turtle.select(_slotRoof)
+  local isRight = true
+  for i=1, length do
+    
+    for j=1, width do
+      turtle.placeDown()
+      if j~= width then
+        turtle.forward()
+      end
+    end -- end for (j)
+
+    if isRight then
+      turtle.turnRight()
+      turtle.forward()
+      turtle.turnRight()
+      isRight = false
+    else
+      turtle.turnLeft()
+      turtle.forward()
+      turtle.turnLeft()
+      isRight = true
+    end
+    refillAll()
+    turtle.select(_slotRoof)
+  end --end for (i)
+
+end -- end roof
+
+--main build function
+function buildHouse(length, width)
+  
+  print(turtle.getFuelLevel())
+  
+  if (length%2~=1 or width%2~=1) then
+    print("Length and width must not be a multiple of 2 value")
+    return
+  end
+  
+  buildLayer1(length, width)
+  buildLayer2(length, width)
+  buildLayer3(length, width)
+  buildLayer4(length, width)
+  buildRoof(length, width)
+
+end -- end buildHouse
+
+--start point
+buildHouse(_length, _width)
